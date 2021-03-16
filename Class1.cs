93c4c7511b2ab16b54d315adc821ce94f0dcb4d9
Raven-Suastegui <80730277+Raven-Suastegui@users.noreply.github.com>
2021-03-15 20:39:56 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C_Areas
+{
+    public class Class1
+    {
+        public decimal Numero01 { get; set; }
+        public decimal Numero02 { get; set; }
+        public decimal A_Cuadrado()
+        {
+            return Numero01 * Numero02;        
+        }
+        public decimal A_Circulo(decimal N01)
+        {
+            Numero01 = N01;
+            double Pi, N;
+            Pi = 3.1416;
+            N = Convert.ToDouble (Numero01);
+            //Pi = convert.todouble(3.1416);
+            return Convert.ToDecimal(Pi * N * N);
+        }
+        public decimal A_Triangulo(decimal N01, decimal N02)
+        {
+            Numero01 = N01;
+            Numero02 = N02;
+            var Resultado = ((N01 * N02) / 2);
+            return Resultado;
+        }
+    }
+}
---
+++ WebService1.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace C_Areas
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public decimal A_Cuadrado(decimal N01, decimal N02)
+        {
+            Class1 c = new Class1();
+            c.Numero01 = N01;
+            c.Numero02 = N02;
+            return c.A_Cuadrado();
+        }
+        [WebMethod]
+        public decimal A_Circulo(decimal N01)
+        {
+            Class1 c = new Class1();
+            return c.A_Circulo(N01);
+        }
+        [WebMethod]
+        public decimal A_triangulo(decimal N01, decimal N02)
+        {
+            Class1 c = new Class1();
+            return c.A_Triangulo(N01,N02);
+        }
+
+    }
+}
